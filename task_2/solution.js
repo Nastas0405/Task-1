bcd5e50e3f27c5244a6eec865bbb79652451e290
Anastasia <n.s.pcd95@gmail.com>
2022-12-24 08:50:53 +0500

    task 2

--- task_2/solution.js
+++ task_2/solution.js
@@ -6,6 +6,12 @@ function calcShipping(sum, min, shipping) {
     // Задание №2.1. Рассчитать доставку
 
     // создайте переменную shippingSum
+    let shippingSum = 0;
+    if (productsSum == 0 || productsSum >= freeShippingMinSum) {
+    shippingSum = 0;
+    } else {
+    shippingSum = shippingPrice;
+    }   
 
     // если productsSum равно 0,
     // то shippingSum присвоить значение 0
@@ -27,9 +33,14 @@ function calcDiscount(sum, min, discount) {
     let discountPart = discount; // величина скидки в процентах
 
     // Задание №2.2. Рассчитать скидку
-
+    
     // создайте переменную discountSum
-
+    let discountSum = 0;
+    if (productsSum >= discountMinSum) {
+        discountSum = discountPart * productsSum / 100;
+        } else {
+        discountSum = 0;
+        }   
     // если productsSum больше или равно discountMinSum,
     // то присвойте discountSum значение discountPart процентов от productsSum,
     // иначе присвойте discountSum значение 0
@@ -46,18 +57,21 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     // Задача №2.3. Рассчитать скидки и доставку в корзине
 
     // создайте переменную totalSum
+    let totalSum = 0;
+    totalSum = productsSum - discountSum;
+
 
     // присвойте totalSum значение productsSum
     // уменьшите totalSum на discountSum
 
     let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
-
+    totalSum = totalSum + shippingSum
     // прибавьте к totalSum значение shippingSum
-
+    let freeShipping = shippingSum == 0;
     // создайте переменную freeShipping
     // запишите без использования if или любых других условий:
     // если shippingSum равно нулю, то freeShipping должна быть равна true, иначе freeShipping должна быть равна false
-
+    
     // Конец решения задачи №2.3.
 
     return {discount: discountSum, freeShipping, shipping: shippingSum, total: totalSum};
